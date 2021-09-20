60556a3e8d9e44fe6e4f57b95d4aa8de44091d9e
Andy Gregorowicz <andrewg@mitre.org>
2021-09-20 17:07:10 -0400

    Making the data copying script multithreaded

--- update-data-set.py
+++ update-data-set.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from pathlib import Path
 from zipfile import ZipFile, ZIP_BZIP2
+import multiprocessing as mp
 
 # This script will take Synthea CSV output files, compress them and
 # put them in the data-set directory. The source directory is passed
@@ -20,12 +21,18 @@ def parse_args():
 def main(args):
   output_dir = Path('data-set')
   csv_files = filter(lambda f: f.endswith('.csv'), os.listdir(args.inputdir))
-  for csv_file in csv_files:
-    csv_file_path = Path(args.inputdir, csv_file)
-    output_file_name = csv_file_path.stem + '.zip'
-    with ZipFile(Path(output_dir, output_file_name), 'w', compression=ZIP_BZIP2) as compressed_csv:
-      compressed_csv.write(csv_file_path, os.path.basename(csv_file))
+  with mp.Pool(processes=8) as pool:
+    for csv_file in csv_files:
+      pool.apply_async(handle_file, (csv_file, args.inputdir, output_dir))
+    pool.close()
+    pool.join()
 
+
+def handle_file(csv_file, inputdir, output_dir):
+  csv_file_path = Path(inputdir, csv_file)
+  output_file_name = csv_file_path.stem + '.zip'
+  with ZipFile(Path(output_dir, output_file_name), 'w', compression=ZIP_BZIP2) as compressed_csv:
+    compressed_csv.write(csv_file_path, os.path.basename(csv_file))
 
 if __name__ == "__main__":
     args = parse_args()
